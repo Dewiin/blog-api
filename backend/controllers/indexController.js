de508db9dd477698a87f0da646420ce2f68001c2
Devin Xie <devinxie418@gmail.com>
2026-02-27 22:43:07 -0500

    implement indexController

--- backend/controllers/indexController.js
+++ backend/controllers/indexController.js
@@ -1,10 +1,33 @@
-import prisma from "../config/prismaClient";
+import { prisma } from "../config/prismaClient";
 
+
+// TODO: add filters
 async function getPosts(req, res) {
     try {
+        const posts = await prisma.post.findMany({
+            where: {
+                published: true,
+            },
+            include: {
+                author: true,
+                comments: {
+                    include: {
+                        user: true,
+                    }
+                }
+            }
+        });
 
+        return res.json({
+            status: 1,
+            posts,
+        });
     } catch {
-
+        console.error("failed to fetch posts from server.");
+        return res.json({
+            status: 0,
+            error: "failed to fetch posts from server.",
+        });
     } 
 }
 
